b7faa71284ae612dabf29ecfbe5f787e97df81f9
idnandre <andre@idntimes.com>
2024-06-05 10:33:12 +0700

    init observability component

---
+++ http/http.go
@@ -0,0 +1,188 @@
+package http
+
+import (
+	"context"
+	"log"
+	"runtime/metrics"
+	"strings"
+	"sync"
+
+	"github.com/idnandre/go-obsv/internal/metadata"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	otelmetric "go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+var (
+	Tracer        trace.Tracer
+	meterProvider *sdkmetric.MeterProvider
+	traceProvider *sdktrace.TracerProvider
+	once          sync.Once
+)
+
+func New(ctx context.Context, otlpHttpTarget, serviceName string) {
+	once.Do(func() {
+		exp, err := newOTLPTraceExporter(ctx, otlpHttpTarget)
+		if err != nil {
+			log.Fatalf("failed to initialize exporter: %v", err)
+		}
+		traceProvider = newTraceProvider(exp, serviceName)
+		Tracer = traceProvider.Tracer(serviceName)
+
+		expM, err := newOTLPMetricExporter(ctx, otlpHttpTarget)
+		if err != nil {
+			log.Fatalf("failed to initialize exporter: %v", err)
+		}
+		meterProvider = newMeterProvider(expM)
+		addMetricsToOTEL(meterProvider, serviceName)
+	})
+}
+
+func Shutdown(ctx context.Context) {
+	go func(ctx context.Context) {
+		traceProvider.Shutdown(ctx)
+	}(ctx)
+	go func(ctx context.Context) {
+		meterProvider.Shutdown(ctx)
+	}(ctx)
+}
+
+// OTLP Trace Exporter
+func newOTLPTraceExporter(ctx context.Context, otlpHttpTarget string) (sdktrace.SpanExporter, error) {
+	// Change default HTTPS -> HTTP
+	insecureOpt := otlptracehttp.WithInsecure()
+
+	// Update default OTLP reciver endpoint
+	endpointOpt := otlptracehttp.WithEndpoint(otlpHttpTarget)
+
+	return otlptracehttp.New(ctx, insecureOpt, endpointOpt)
+}
+
+// OTLP Metric Exporter
+func newOTLPMetricExporter(ctx context.Context, otlpHttpTarget string) (sdkmetric.Exporter, error) {
+	// Change default HTTPS -> HTTP
+	insecureOpt := otlpmetrichttp.WithInsecure()
+
+	// Update default OTLP reciver endpoint
+	endpointOpt := otlpmetrichttp.WithEndpoint(otlpHttpTarget)
+
+	return otlpmetrichttp.New(ctx, insecureOpt, endpointOpt)
+}
+
+// TracerProvider is an OpenTelemetry TracerProvider.
+// It provides Tracers to instrumentation so it can trace operational flow through a system.
+func newTraceProvider(exp sdktrace.SpanExporter, serviceName string) *sdktrace.TracerProvider {
+	// Ensure default SDK resources and the required service name are set.
+	r, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+		),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exp),
+		sdktrace.WithResource(r),
+	)
+}
+
+func newMeterProvider(exp sdkmetric.Exporter) *sdkmetric.MeterProvider {
+	extraResources, _ := resource.New(
+		context.Background(),
+		resource.WithOS(),
+		resource.WithProcess(),
+		resource.WithContainer(),
+		resource.WithHost(),
+	)
+	resource, _ := resource.Merge(
+		resource.Default(),
+		extraResources,
+	)
+
+	return sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exp)),
+		sdkmetric.WithResource(resource),
+	)
+}
+
+func addMetricsToOTEL(provider *sdkmetric.MeterProvider, serviceName string) {
+	meter := provider.Meter(serviceName)
+
+	// Get descriptions for all supported metrics.
+	metricsMeta := metrics.All()
+
+	// Register metrics and retrieve the values in prometheus client
+	for i := range metricsMeta {
+		// Get metric options
+		meta := metricsMeta[i]
+		opt := getMetricsOptions(metricsMeta[i])
+		name := normalizeOtelName(meta.Name)
+
+		// Register metrics per type of metric
+		if meta.Cumulative {
+			// Register as a counter
+			counter, err := meter.Float64ObservableCounter(name, otelmetric.WithDescription(meta.Description))
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = meter.RegisterCallback(func(_ context.Context, o otelmetric.Observer) error {
+				o.ObserveFloat64(counter, metadata.GetSingleMetricFloat(meta.Name), opt)
+				return nil
+			}, counter)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			// Register as a gauge
+			gauge, err := meter.Float64ObservableGauge(name, otelmetric.WithDescription(meta.Description))
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = meter.RegisterCallback(func(_ context.Context, o otelmetric.Observer) error {
+				o.ObserveFloat64(gauge, metadata.GetSingleMetricFloat(meta.Name), opt)
+				return nil
+			}, gauge)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+// getMetricsOptions function to get metric labels
+func getMetricsOptions(metric metrics.Description) otelmetric.MeasurementOption {
+	tokens := strings.Split(metric.Name, "/")
+	if len(tokens) < 2 {
+		return nil
+	}
+
+	nameTokens := strings.Split(tokens[len(tokens)-1], ":")
+	subsystem := metadata.GetMetricSubsystemName(metric)
+
+	// create a unique name for metric, that will be its primary key on the registry
+	opt := otelmetric.WithAttributes(
+		attribute.Key("Namespace").String(tokens[1]),
+		attribute.Key("Subsystem").String(subsystem),
+		attribute.Key("Units").String(nameTokens[1]),
+	)
+	return opt
+}
+
+// normalizePrometheusName function to normalize prometheus metric name
+func normalizeOtelName(name string) string {
+	normalizedName := strings.Replace(name, "/", "", 1)
+	normalizedName = strings.Replace(normalizedName, ":", "_", -1)
+	normalizedName = strings.TrimSpace(strings.ReplaceAll(normalizedName, "/", "_"))
+	return normalizedName
+}
---
+++ http/middleware/middleware.go
@@ -0,0 +1,49 @@
+package nethttp
+
+import (
+	"net/http"
+
+	obsvhttp "github.com/idnandre/go-obsv/http"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func TraceMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, span := obsvhttp.Tracer.Start(r.Context(), r.Method+" "+r.URL.Path, trace.WithSpanKind(trace.SpanKindServer))
+		defer span.End()
+
+		newRequest := r.WithContext(ctx)
+		newResponseWriter := newResponseWriter(w)
+
+		next.ServeHTTP(newResponseWriter, newRequest)
+
+		span.SetAttributes(
+			attribute.String("span.kind", "server"),
+			attribute.String("resource.name", r.Method+" "+r.URL.Path),
+			attribute.String("http.method", r.Method),
+			attribute.String("http.url", r.URL.Path),
+			attribute.String("http.raw.query", r.URL.RawQuery),
+			attribute.String("http.route", r.URL.Path),
+			attribute.String("http.target", r.URL.String()),
+			attribute.String("http.useragent", r.UserAgent()),
+			attribute.String("http.host", r.Host),
+			attribute.Int("http.status_code", newResponseWriter.statusCode),
+		)
+	})
+}
