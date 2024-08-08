f5a3b84dee3ad71eae7ccb4504e6947d91d9417e
idnandre <andre@idntimes.com>
2024-08-08 10:15:09 +0700

    add handle lambda from HTTP API Gateway

---
+++ lambda/middlewarev2/lambda.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"context"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/idnandre/gobsv/lambda"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type handlerFunc func(context.Context, events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error)
+
+func TraceMiddleware(f handlerFunc) handlerFunc {
+	return func(ctx context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+		routPattern := event.RouteKey
+
+		headers := make(map[string][]string)
+		for key, values := range event.Headers {
+			valuesArray := strings.Split(values, ",")
+			headers[key] = valuesArray
+		}
+
+		newCtx := otel.GetTextMapPropagator().Extract(ctx, propagation.HeaderCarrier(headers))
+		newCtx, span := otel.Tracer("").Start(newCtx, event.RequestContext.HTTP.Method+" "+routPattern, trace.WithSpanKind(trace.SpanKindServer))
+		defer lambda.ForceFlush(newCtx)
+		defer span.End()
+
+		response, err := f(newCtx, event)
+
+		span.SetAttributes(
+			attribute.String("span.kind", "server"),
+			attribute.String("resource.name", event.RequestContext.HTTP.Method+" "+event.RawPath),
+			attribute.String("http.method", event.RequestContext.HTTP.Method),
+			attribute.String("http.url", routPattern),
+			attribute.String("http.raw.query", event.RawQueryString),
+			attribute.String("http.route", routPattern),
+			attribute.String("http.target", routPattern),
+			attribute.String("http.useragent", event.RequestContext.HTTP.UserAgent),
+			attribute.Int("http.status_code", response.StatusCode),
+		)
+
+		return response, err
+
+	}
+}
